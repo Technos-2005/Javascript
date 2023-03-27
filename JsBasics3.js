aa365b229d762d79e631dd77601612994f3a6732
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-27 19:48:05 +0530

    Basic Program

---
+++ JsBasics3.js
@@ -0,0 +1,36 @@
+/* let age = prompt("What is your age?");
+if(age>10 && age<20){
+  console.log("Your age lies between 10 and 20.")
+} else {
+  console.log("Your age doesn't lies between 10 and 20.")
+} */
+
+
+/*
+let age = prompt("What is your age?");
+switch(age){
+  case '10' : console.log("Your age is 10.")
+    break;
+  case '11' : console.log("Your age is 11.")
+    break;
+  case '12' : console.log("Your age is 12.")
+    break;
+  case '13' : console.log("Your age is 13.")
+    break;
+  default : console.log("Your age is not defined.")
+    break;
+} */
+
+/*
+let num = prompt("Enter Num : ")
+if(num%2==0 && num%3==0){
+  console.log("Your Num is divisible by 2 and 3.")
+} else {
+  console.log("Your Num is not divisible by 2 and 3.")
+} */
+
+/*
+let age = prompt("What is your age?")
+let a = age>=18?"You can Drive.":"You cannot Drive.";
+console.log(a)
+*/
