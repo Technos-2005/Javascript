ceb48938f069954fd8b80bf96c641b1001b1e108
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-04-01 18:43:17 +0530

    Using For Loop

---
+++ JsBasics6.js
@@ -0,0 +1,11 @@
+let marks = {
+    Nachiket: 99,
+    Divy: 95,
+    Het: 96,
+    Abhi: 97
+  }
+  
+  for(let i=0;i<Object.keys(marks).length;i++){
+    console.log(" The marks of " + Object.keys(marks)[i] + " are " 
+   +marks[Object.keys(marks)[i]])
+  }
