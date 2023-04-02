61c3c850bf3c6252dc1d7e9eb005f1d40f1a75d1
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-04-02 18:52:48 +0530

    String Chapter

---
+++ JsBasics6.js
@@ -0,0 +1,22 @@
+let name = "Nachiket"
+console.log(name)
+console.log(name.length)
+
+let name1 = 'Rudra'
+console.log(name1)
+
+//Template Literals
+let boy1 = "Nachiket"
+let boy2 = "Rudra"
+//Nachiket is a brother of Rudra
+let sentence = `${boy1} is a brother of ${boy2}`
+console.log(sentence)
+
+//Escape Sequence Characters
+let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length)
+
+let fruit1 = "Bana\"na"
+console.log(fruit1)
+console.log(fruit1.length)
