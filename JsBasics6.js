cbe51a57d832831520695ad96af81773476919a8
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-04-03 21:45:42 +0530

    Array Chapter

--- JsBasics6.js
+++ JsBasics6.js
@@ -5,7 +5,14 @@ let marks = {
     Abhi: 97
   }
   
-  for(let i=0;i<Object.keys(marks).length;i++){
-    console.log(" The marks of " + Object.keys(marks)[i] + " are " 
-   +marks[Object.keys(marks)[i]])
+//   for(let i=0;i<Object.keys(marks).length;i++){
+//     console.log(" The marks of " + Object.keys(marks)[i] + " are " 
+//    +marks[Object.keys(marks)[i]])
+//   }
+
+  for(let key in marks){
+    console.log(" The marks of " + key + " are " + marks[key])
+//     console.log(" The marks of " + Object.keys(marks)[key] + " are " 
+//    +marks[Object.keys(marks)[key]])
   }
+
